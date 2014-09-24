9801f4d0d5e390a6b3e6871562bbcc65b87ae09e
Georgios Koloventzos <gk2409@columbia.edu>
2014-09-23 21:38:56 -0400

    start of incrementing dfs

--- kernel/kernel/ptree.c
+++ kernel/kernel/ptree.c
@@ -12,13 +12,15 @@
 #include <asm-generic/errno-base.h>
 
 static void dfs(struct prinfo *, struct task_struct *, int *, int *);
+static void full_prinfo(struct prinfo *, struct task_struct *);
 
 
 asmlinkage int sys_ptree(struct prinfo *buf, int *nr)
 {
 	int errno;
+	struct task_struct *ega_task, *cur;
 	struct prinfo *kbuf;
-        int iterations;
+        int iterations, store;
 
 	if (buf == NULL || nr == NULL) {
 		errno = -EINVAL;
@@ -35,8 +37,30 @@ asmlinkage int sys_ptree(struct prinfo *buf, int *nr)
         }
         printk(KERN_ERR "Here I am: %d\n", *nr);
         read_lock(&tasklist_lock);
-        iterations = 0;
-        dfs(kbuf, &(init_task), &iterations, nr);
+	ega_task = &init_task;
+	interations = 0;
+	store = 1;
+	while (interations <= *nr) {
+		cur = ega_task;
+		if (store) {
+			full_prinfo(buf + iterations, cur);
+			iterations++;
+		}
+		store = 1;
+		if (!list_empty(cur->children)) {
+			ega_task = list_entry(cur->children->next,\
+				   struct task_struct, children);
+			continue;
+		} else {
+			/* Go to next sibling*/
+		}
+		if (!list_empty(cur->siblings)){
+			ega_task = cur->p_cptr;
+			continue;	
+		}
+		
+
+	}
 	read_unlock(&tasklist_lock);
 	if (copy_to_user(buf, kbuf, *nr * sizeof(struct prinfo))) {
  		errno = -EFAULT;
@@ -48,6 +72,24 @@ error:
 	return errno;
 }
 
+
+void full_prinfo(struct prinfo *cur, struct task_struct *tsk)
+{
+
+	cur->parent_pid = tsk->real_parent->pid;
+	cur->pid = tsk->pid;
+	cur->state = tsk->state;
+	cur->uid = (long) tsk->cred->uid;
+	cur->state = tsk->state;
+	get_task_comm(cur->comm, tsk);
+        /*                                                                      
+         * cur->next_sibling_pid = tsk->p_ysptr; younger sibling                
+         * cur->first_child_pid = tsk->p_cptr; youngest child                   
+         * cur->uid = (long) tsk->cred->uid;                                    
+         */ 
+	return ;
+
+}
 
 void dfs(struct prinfo *buf, struct task_struct *tsk, int *iter, int *nr)
 {

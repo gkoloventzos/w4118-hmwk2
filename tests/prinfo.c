5b42091654f17d073284e6557b2ae3f0c62c421b
Vaggelis Atlidakis <vatlidak@cs.columbia.edu>
2014-09-26 16:00:28 -0400

    Add demonstrating program

---
+++ tests/prinfo.c
@@ -0,0 +1,76 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include "prinfo.h"
+
+/*
+ * Helper function to print process info add necessary tabs.
+ */
+void print_process(struct prinfo p, int depth)
+{
+	int i;
+
+	for (i = 0; i < depth; i++)
+		printf("\t");
+	printf("%s, %ld, %ld, %ld, %ld, %ld, %ld\n", p.comm, (long) p.pid,
+	       p.state, (long) p.parent_pid, (long) p.first_child_pid,
+	       (long) p.next_sibling_pid, p.uid);
+
+}
+
+int main(int argc, char **argv)
+{
+	int i;
+	int rval;
+	int nproc;
+	int depth;
+	pid_t parent_pid;
+	struct prinfo *buf;
+
+	if (argc != 2) {
+		printf("Usage:%s  <number of processes\n>", argv[0]);
+		goto error;
+	}
+	nproc = atoi(argv[1]);
+	buf = calloc(nproc, sizeof(struct prinfo));
+	if (buf == NULL) {
+		perror("calloc:");
+		 goto error;
+	}
+	 rval = syscall(223, buf, &nproc);
+	if (rval < 0) {
+		perror("ptree:");
+		goto error_free_mem;
+	}
+	/*
+	 *Your system call should return the total number of entries on
+	 *success (this may be bigger than the actual number of entries copied)
+	 * seg fault?
+	 */
+	print_process(buf[0], 0);
+	parent_pid = -1;
+	depth = 1;
+	for (i = 0; i != nproc; i++) {
+		if (parent_pid == buf[i].parent_pid) {
+			print_process(buf[i], depth);
+			continue;
+		}
+		if (buf[i].parent_pid != buf[i - 1].pid) {
+			depth++;
+			parent_pid = buf[i].parent_pid;
+			print_process(buf[i], depth);
+			continue;
+		}
+		--depth;
+		parent_pid = buf[i].parent_pid;
+		print_process(buf[i], depth);
+	}
+	return 0;
+error_free_mem:
+	free(buf);
+error:
+	return -1;
+}
+
+
